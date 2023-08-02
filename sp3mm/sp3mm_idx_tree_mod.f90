c713876fdc8415fbe715f7570fa7d669ffd29d51
wlthr <dwalther@enseirb-matmeca.fr>
2023-08-02 15:10:34 +0200

    switched from upperbound with dense arrays to red-black tree

---
+++ sp3mm/sp3mm_idx_tree_mod.f90
@@ -0,0 +1,320 @@
+module idx_tree_mod
+    use psb_base_mod
+
+    implicit none
+    type :: idx_node
+        integer(psb_ipk_) :: idx
+        real(psb_dpk_) :: val
+        type(idx_node), pointer :: left, right, parent
+        logical :: is_red
+    end type idx_node
+
+    type :: idx_tree
+        type(idx_node), pointer :: root
+        integer(psb_ipk_) :: nnz
+
+        contains
+
+        procedure :: insert => insert_tree
+    end type idx_tree
+
+    contains
+
+    subroutine insert_tree(this, idx, val)
+        implicit none
+        class(idx_tree), intent(inout) :: this
+        integer(psb_ipk_), intent(in) :: idx
+        real(psb_dpk_), intent(in) :: val
+
+        type(idx_node), pointer :: new_node
+        type(idx_node), pointer :: current, previous
+        
+        allocate(new_node)
+        new_node%idx = idx
+        new_node%val = val
+        nullify(new_node%left)
+        nullify(new_node%right)
+        nullify(new_node%parent)
+        new_node%is_red = .true.
+
+
+        if (.not. associated(this%root)) then
+            this%root => new_node
+            this%nnz = 1
+            new_node%is_red = .false.
+            return
+        end if
+
+        current => this%root
+
+        do while (associated(current))
+            previous => current
+
+            if (idx == current%idx) then
+                current%val = current%val + val
+                deallocate(new_node)
+                return
+            else if (idx < current%idx) then
+                current => current%left
+            else
+
+                current => current%right
+            end if
+        end do
+
+        if (idx < previous%idx) then
+            new_node%parent => previous
+            previous%left => new_node
+        else
+            new_node%parent => previous
+            previous%right => new_node
+        end if
+
+        call fix_insertion(this, new_node)
+
+        this%nnz = this%nnz + 1
+    end subroutine insert_tree
+
+    subroutine fix_insertion(this, node)
+        class(idx_tree), intent(inout) :: this
+        type(idx_node), pointer, intent(inout) :: node
+
+        type(idx_node), pointer :: current, parent, grand_parent, uncle
+
+        current => node
+        parent => current%parent
+        do while(associated(parent) .and. parent%is_red)
+            ! grand parent exist because root can't be red
+            grand_parent => parent%parent
+            if (parent%idx < grand_parent%idx) then
+                uncle => grand_parent%right
+            else
+                uncle => grand_parent%left
+            end if
+
+            if (associated(uncle) .and. uncle%is_red) then
+                parent%is_red = .false.
+                uncle%is_red = .false.
+                grand_parent%is_red = .true.
+                current => grand_parent
+                parent => current%parent
+
+            ! Left-Left case
+            else if (current%idx < parent%idx .and. &
+                parent%idx < grand_parent%idx) then
+                call rotate_right(grand_parent)
+                call swap_colors(parent, grand_parent)
+
+                if (this%root%idx == grand_parent%idx) this%root => parent
+
+                return
+            ! Left-Right case
+            else if (current%idx > parent%idx .and. &
+                parent%idx < grand_parent%idx) then
+                call rotate_left(parent)
+                call rotate_right(grand_parent)
+                call swap_colors(current, grand_parent)
+
+                if (this%root%idx == grand_parent%idx) this%root => current
+
+                return
+            ! Right-Right case
+            else if (current%idx > parent%idx .and. &
+                parent%idx > grand_parent%idx) then
+                call rotate_left(grand_parent)
+                call swap_colors(parent, grand_parent)
+
+                if (this%root%idx == grand_parent%idx) this%root => parent
+
+                return
+            ! Right-Left case
+            else
+                call rotate_right(parent)
+                call rotate_left(grand_parent)
+                call swap_colors(current, grand_parent)
+
+                if (this%root%idx == grand_parent%idx) this%root => current
+                
+                return
+            end if
+        end do
+
+        this%root%is_red = .false.
+    end subroutine fix_insertion
+
+    subroutine swap_colors(n1, n2)
+        type(idx_node), pointer, intent(inout) :: n1, n2
+
+        logical :: tmp
+
+        tmp = n1%is_red
+        n1%is_red = n2%is_red
+        n2%is_red = tmp
+    end subroutine swap_colors
+    
+    subroutine rotate_right(node)
+        type(idx_node), pointer, intent(inout) :: node
+
+        type(idx_node), pointer :: l, lr
+
+        if (.not. associated(node%left)) return
+
+        l => node%left
+        lr => l%right
+        node%left => lr
+
+        if (associated(lr)) lr%parent => node
+
+        if (associated(node%parent)) then
+            if (node%idx < node%parent%idx) then
+                node%parent%left => l
+            else
+                node%parent%right => l
+            end if
+        end if
+
+        l%parent => node%parent
+        node%parent => l
+
+        l%right => node
+    end subroutine rotate_right
+
+    subroutine rotate_left(node)
+        type(idx_node), pointer, intent(inout) :: node
+
+        type(idx_node), pointer :: r, rl
+
+        if (.not. associated(node%right)) return
+
+        r => node%right
+        rl => r%left
+        node%right => rl
+
+        if (associated(rl)) rl%parent => node
+
+        if (associated(node%parent)) then
+            if (node%idx < node%parent%idx) then
+                node%parent%left => r
+            else
+                node%parent%right => r
+            end if
+        end if
+
+        r%parent => node%parent
+        node%parent => r
+
+        r%left => node
+    end subroutine rotate_left
+    
+    subroutine scalar_sparse_row_mul(tree, scalar, mat, row_num)
+        implicit none
+        type(idx_tree), intent(inout) :: tree
+        real(psb_dpk_), intent(in) :: scalar
+        type(psb_d_csr_sparse_mat), intent(in) :: mat
+        integer(psb_ipk_), intent(in) :: row_num
+
+        integer(psb_ipk_) :: i
+
+        do i = mat%irp(row_num), mat%irp(row_num + 1) - 1
+            call tree%insert(mat%ja(i),scalar * mat%val(i))
+        end do
+
+    end subroutine scalar_sparse_row_mul
+
+    subroutine merge_trees(trees, mat)
+        use omp_lib
+        type(idx_tree), allocatable, intent(inout) :: trees(:)
+        type(psb_d_csr_sparse_mat), intent(inout) :: mat
+
+        integer(psb_ipk_) :: i, j, rows, info
+        type(idx_node), pointer :: current, previous
+
+        rows = size(trees)
+
+        mat%irp(1) = 1
+        
+        do i=1, rows
+            mat%irp(i + 1) = mat%irp(i) + trees(i)%nnz
+        end do
+
+        call psb_realloc(mat%irp(rows + 1), mat%val, info)
+        call psb_realloc(mat%irp(rows + 1), mat%ja, info)
+
+        do i = 1, size(trees)
+            j = 0
+            current => trees(i)%root
+            do while(associated(current))
+                ! go to the left-most node
+                do while(associated(current%left))
+                    current => current%left
+                end do
+                mat%val(j + mat%irp(i)) = current%val
+                mat%ja(j + mat%irp(i)) = current%idx
+                j = j + 1
+
+                previous => current
+                if (associated(current%right)) then
+                    if (associated(current%parent)) then
+                        current%parent%left => current%right
+                    end if
+                    current%right%parent => current%parent
+                    current => current%right
+                else
+                    current => current%parent
+                    if (associated(current)) nullify(current%left)
+                end if
+                deallocate(previous)
+            end do
+        end do
+    end subroutine merge_trees
+
+    subroutine merge_trees_distrib(trees, mat)
+        use omp_lib
+        type(idx_tree), allocatable, intent(inout) :: trees(:)
+        type(psb_d_csr_sparse_mat), intent(inout) :: mat
+
+        integer(psb_ipk_) :: i, j, rows, info
+        type(idx_node), pointer :: current, previous
+
+        rows = size(trees)
+
+        mat%irp(1) = 1
+        
+        do i=1, rows
+            mat%irp(i + 1) = mat%irp(i) + trees(i)%nnz
+        end do
+
+        call psb_realloc(mat%irp(rows + 1), mat%val, info)
+        call psb_realloc(mat%irp(rows + 1), mat%ja, info)
+
+        !$omp parallel do schedule(runtime), private(current, previous, j)
+        do i = 1, size(trees)
+            j = 0
+            current => trees(i)%root
+            do while(associated(current))
+                ! go to the left-most node
+                do while(associated(current%left))
+                    current => current%left
+                end do
+                mat%val(j + mat%irp(i)) = current%val
+                mat%ja(j + mat%irp(i)) = current%idx
+                j = j + 1
+
+                previous => current
+                if (associated(current%right)) then
+                    if (associated(current%parent)) then
+                        current%parent%left => current%right
+                    end if
+                    current%right%parent => current%parent
+                    current => current%right
+                else
+                    current => current%parent
+                    if (associated(current)) nullify(current%left)
+                end if
+                deallocate(previous)
+            end do
+        end do
+        !$omp end parallel do
+    end subroutine merge_trees_distrib
+    
+end module idx_tree_mod
--- sp3mm/spmm_mod.f90
+++ sp3mm/spmm_mod.f90
@@ -2,7 +2,7 @@ module spmm_mod
     use omp_lib
     use psb_base_mod
     use sp3mm_config_mod
-    use sp3mm_acc_mod
+    use idx_tree_mod
     implicit none
 
     contains
@@ -49,30 +49,6 @@ module spmm_mod
             integer(psb_ipk_), allocatable, intent(out):: rows_parts_sizes(:)
             integer(psb_ipk_), intent(out) :: info
 
-            integer(psb_ipk_) :: full_mat_bound
-            integer(psb_ipk_) :: row, jj, j, rlen, col_group, b_part_id
-
-            call psb_realloc(a%get_nrows() * cols + 1, rows_parts_sizes, info)
-
-            rows_parts_sizes = 0
-
-            !omp parallel do schedule(static) reduction(+:full_mat_bound)
-            do row = 0, a%get_nrows() - 1
-                do jj = a%irp(row), a%irp(row + 1) - 1
-                    j = a%ja(jj)
-                    b_part_id = col_group + j * cols + 1
-                    do col_group = 0, cols - 1
-                        rlen = b_col_offsets(b_part_id + 1) - b_col_offsets(b_part_id)
-                        rows_parts_sizes(col_group + row * cols + 1) = rlen + &
-                                    rows_parts_sizes(col_group + row * cols + 1)
-                        full_mat_bound = full_mat_bound + rlen
-                    end do
-                end do
-            end do
-            !omp end parallel do
-
-            rows_parts_sizes(a%get_nrows() * cols + 1) = full_mat_bound
-
         end subroutine spmm_size_uperbound_col_parts
         
         subroutine spmm_serial(a,b,c,cfg,info)
@@ -83,8 +59,8 @@ module spmm_mod
             integer(psb_ipk_), intent(out) :: info
 
             integer(psb_ipk_)   :: a_m, b_n
-            type(dense_acc)    :: acc
             integer(psb_ipk_)   :: row, col
+            type(idx_tree), allocatable :: row_accs(:)
 
             a_m = a%get_nrows()
             b_n = b%get_ncols()
@@ -92,21 +68,21 @@ module spmm_mod
             write (*, '("spmm", A, "rows of A,", A, "full B", A, "M=", I0, " x N=", I0)')&
             achar(9), achar(9), achar(9), a_m, b_n
 
-            call c%allocate_mnnz(a_m, b_n)
+            allocate(row_accs(a_m))
+            call c%allocate(a_m, b_n)
 
-            c%irp(1) = 1
-
-            call acc%init(b_n)
-
-            do row=1, a_m
+            do row = 1, a_m
+                row_accs(row)%nnz = 0
+                nullify(row_accs(row)%root)
                 do col = a%irp(row), a%irp(row + 1) - 1
-                    call scalar_sparse_row_mul(acc, a%val(col), b, a%ja(col))
-                end do
-                call sparsify_direct(acc, c, row)
-                call acc%reset()
+                    call scalar_sparse_row_mul(row_accs(row), a%val(col), b, a%ja(col))
+                end do 
             end do
+            call merge_trees(row_accs, c)
 
-        info = 0
+            deallocate(row_accs)
+
+            info = 0
         end subroutine spmm_serial
 
         subroutine spmm_row_by_row(a,b,c,cfg,info)
@@ -117,12 +93,9 @@ module spmm_mod
             integer(psb_ipk_), intent(out) :: info
 
             integer(psb_ipk_)   :: a_m, b_n
-            type(dense_acc), allocatable :: accs(:)
-            type(dense_acc) :: acc
-            type(sparse_acc), allocatable:: sp_accs(:)
-            integer(psb_ipk_)   :: i, row, col
-            integer(psb_ipk_), allocatable :: rows_sizes(:)
-            real(8) :: tic, toc
+            integer(psb_ipk_)   :: row, col
+            type(idx_tree), allocatable :: row_accs(:)
+
             a_m = a%get_nrows()
             b_n = b%get_ncols()
 
@@ -131,40 +104,22 @@ module spmm_mod
 
             call c%allocate(a_m, b_n)
 
-            c%irp(1) = 1
+            allocate(row_accs(a_m))
+            call c%allocate(a_m, b_n)
 
-            ! call spmm_size_uperbound(a, b, rows_sizes, info)
-
-            ! allocating the dense accumulators
-            allocate(accs(cfg%thread_num))
-
-            do i = 1, cfg%thread_num
-                call accs(i)%init(b_n)
-            end do
-
-            ! allocating the sparse accumulators (rows)
-            allocate(sp_accs(a_m))
-
-            call cfg%chunk_distrib_func(a_m, info)
-
-            tic = omp_get_wtime()
-            !$omp parallel do schedule(runtime), private(acc)
-            do row=1, a_m
-                acc = accs(omp_get_thread_num() + 1)
+            !$omp parallel do schedule(runtime)
+            do row = 1, a_m
+                row_accs(row)%nnz = 0
+                nullify(row_accs(row)%root)
                 do col = a%irp(row), a%irp(row + 1) - 1
-                    call scalar_sparse_row_mul(acc, a%val(col), b, a%ja(col))
-                end do
-                call sparsify_ub(acc, sp_accs(row), 0)
-                call acc%reset()
+                    call scalar_sparse_row_mul(row_accs(row), a%val(col), b, a%ja(col))
+                end do 
             end do
             !$omp end parallel do
-            
-            toc = omp_get_wtime()
-            print *, "calculated rows in ", toc - tic
-            tic = omp_get_wtime()
-            call merge_rows(sp_accs, c)
-            toc = omp_get_wtime()
-            print *, "merge rows in ", toc - tic
+            call merge_trees_distrib(row_accs, c)
+
+            deallocate(row_accs)
+            info = 0
         end subroutine spmm_row_by_row
 
         subroutine spmm_row_by_row_1D_blocks(a,b,c,cfg,info)
@@ -175,11 +130,6 @@ module spmm_mod
             integer(psb_ipk_), intent(out) :: info
             
             integer(psb_ipk_)   :: a_m, b_n
-            type(dense_acc), allocatable :: accs(:)
-            type(dense_acc) :: acc
-            type(sparse_acc), allocatable:: sp_accs(:)
-            integer(psb_ipk_)   :: i, row, col
-            integer(psb_ipk_) :: row_block, row_block_rem, block_num, blck, start_row
 
             a_m = a%get_nrows()
             b_n = b%get_ncols()
@@ -191,45 +141,6 @@ module spmm_mod
 
             c%irp(1) = 1
 
-            ! call spmm_size_uperbound(a, b, rows_sizes, info)
-
-            ! allocating the dense accumulators
-            allocate(accs(cfg%thread_num))
-
-            do i = 1, cfg%thread_num
-                call accs(i)%init(b_n)
-            end do
-
-            allocate(sp_accs(a_m))
-
-            row_block = a_m / cfg%rows
-            row_block_rem = mod(a_m, cfg%rows)
-
-            call cfg%chunk_distrib_func(cfg%rows, info)
-
-            !$omp parallel do schedule(runtime) private(acc, start_row, blck)
-            do block_num = 0, cfg%rows - 1
-                if (block_num < row_block_rem) then
-                    blck = row_block + 1
-                else
-                    blck = row_block
-                end if
-
-                start_row = block_num * row_block + min(block_num, row_block_rem)
-                
-                acc = accs(omp_get_thread_num() + 1) 
-                
-                do row = start_row + 1, start_row + blck
-                    do col = a%irp(row), a%irp(row + 1) - 1
-                        call scalar_sparse_row_mul(acc, a%val(col), b, a%ja(col))
-                    end do
-                    call sparsify_ub(acc, sp_accs(row), 0)
-                    call acc%reset()
-                end do
-            end do
-            !$omp end parallel do
-
-            call merge_rows(sp_accs, c)
         end subroutine spmm_row_by_row_1D_blocks
 
         subroutine spmm_row_by_row_2D_blocks(a,b,c,cfg,info)
@@ -240,18 +151,6 @@ module spmm_mod
             integer(psb_ipk_), intent(out) :: info
 
             integer(psb_ipk_)   :: a_m, b_n
-            type(dense_acc), allocatable :: accs(:)
-            type(dense_acc) :: acc
-            type(sparse_acc), allocatable:: sp_accs(:)
-            integer(psb_ipk_) :: i
-            integer(psb_ipk_), allocatable :: b_col_offsets(:)
-            integer(psb_ipk_) :: grid_size, a_sub_row_num, rows_parts_sizes_num
-            integer(psb_ipk_), allocatable :: rows_parts_sizes(:)
-            integer(psb_ipk_) :: row_block, row_block_rem
-            integer(psb_ipk_) :: col_block, col_block_rem
-            ! integer(psb_ipk_) :: row_block, col_block
-            integer(psb_ipk_) :: row_start, col_start
-
 
             a_m = a%get_nrows()
             b_n = b%get_ncols()
@@ -261,65 +160,6 @@ module spmm_mod
 
             call c%allocate(a_m, b_n)
 
-            grid_size = cfg%rows * cfg%cols
-            a_sub_row_num = a_m * cfg%cols
-
-            row_block = a_m / cfg%rows
-            row_block_rem = mod(a_m, cfg%rows)
-
-            col_block = b_n / cfg%cols
-            col_block_rem = mod(b_n, cfg%cols)
-
-            call cols_offset_partitioning_unif_ranges(b, cfg%cols, b_col_offsets)
-
-            rows_parts_sizes_num = a_sub_row_num
-
-            call spmm_size_uperbound_col_parts(a, b, cfg%cols, b_col_offsets, &
-                                                rows_parts_sizes, info)
-
-            allocate(accs(grid_size))
-
-            do i = 1, grid_size
-                if (col_block_rem == 0) then
-                    call accs(i)%init(col_block)
-                else
-                    call accs(i)%init(col_block + 1)
-                end if
-            end do
-
-            allocate(sp_accs(a_m))
-
         end subroutine spmm_row_by_row_2D_blocks
 
-        subroutine cols_offset_partitioning_unif_ranges(mat, grid_cols, offsets)
-            implicit none
-            type(psb_d_csr_sparse_mat), intent(in) :: mat
-            integer(psb_ipk_), intent(in) :: grid_cols
-            integer(psb_ipk_), allocatable, intent(out):: offsets(:)
-
-            integer(psb_ipk_) :: sub_rows_num, col_block, col_block_rem
-            integer(psb_ipk_) :: info
-            integer(psb_ipk_) :: row, j, col_group, col_group_col_start
-
-            sub_rows_num = mat%get_nrows() * grid_cols
-            col_block = mat%get_ncols() / grid_cols
-            col_block_rem = mod(mat%get_ncols(), grid_cols)
-
-            call psb_realloc(sub_rows_num + 1, offsets, info)
-
-            j = 1
-            do row = 0, mat%get_nrows() - 1
-                j = mat%irp(row + 1)
-                offsets(1 + row * grid_cols) = j
-                do col_group = 1, grid_cols - 1
-                    col_group_col_start = col_group * col_block + min(col_group, col_block_rem)
-                    do while (j < mat%irp(row + 2) .and. mat%ja(j) < col_group_col_start)
-                        j = j + 1
-                    end do
-                    offsets(col_group + row * grid_cols + 1) = j
-                end do
-            end do
-
-            offsets(sub_rows_num + 1) = mat%get_nzeros() + 1
-        end subroutine
 end module spmm_mod
